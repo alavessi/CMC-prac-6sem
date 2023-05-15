bdb9bf6e54b7ebe19a9293dc8897ee5f575e15ad
alavessi <84871525+alavessi@users.noreply.github.com>
2023-05-15 22:19:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,14 +6,14 @@ from data_parser import Parser
 from solver import Solver
 from plotter import Plotter
 
+
 def main():
     sympy.init_printing(use_unicode=True)
-    t = sympy.symbols('t')
 
-    parser = Parser(t)
+    parser = Parser()
     data = parser.parse_input_data()
 
-    solver = Solver(data, t, data['x'])
+    solver = Solver(data)
     t, x = solver.solve()
 
     plotter = Plotter(t, x)
