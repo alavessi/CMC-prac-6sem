3177c2c26d2bd3ba535ae23a08ed5983adb976ba
alavessi <84871525+alavessi@users.noreply.github.com>
2023-05-28 01:46:28 +0300

    Update main.py
    Add 3 problems from the book

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
+import argparse
 import sympy
 
-from data_parser import Parser
+from data_parser import Parser, is_number
 from solver import Solver
 from plotter import Plotter
 
@@ -10,12 +11,18 @@ from plotter import Plotter
 def main():
     sympy.init_printing(use_unicode=True)
 
-    parser = Parser()
+    parser = argparse.ArgumentParser(description='Решение кревой задачи методом продолжения по параметру')
+    parser.add_argument('--problem_name', type=str, default='', help='Вы можете выбрать одну из известных задач (по умолчанию данные вводятся в полном объёме с клавиатуры)')
+    args = parser.parse_args()
+
+    parser = Parser(args.problem_name)
     data = parser.parse_input_data()
 
     solver = Solver(data)
     t, x = solver.solve()
-
+    print("t =", t)
+    print("x =", x)
+    
     plotter = Plotter(t, x)
     plotter.plot_x()
 
