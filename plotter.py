9bbd08fa7ecb79c914632276e38076aea5b12189
alavessi <84871525+alavessi@users.noreply.github.com>
2023-06-10 00:38:35 +0300

    Update plotter.py

--- plotter.py
+++ plotter.py
@@ -10,9 +10,11 @@ class Plotter:
         plt.xlabel('t')
         plt.ylabel('x(t)')
         plt.title('x(t) = (x0(t), x1(t), ..., x_n-1(t))')
-        colors = {0: 'red', 1: 'blue', 2: 'green'}
-        for i in range(len(self.x_)):
-            plt.plot(self.t_, self.x_[i], color=colors[i], label=f'x{i}(t)')
+        colors = {0: 'red', 1: 'blue', 2: 'green', 3: 'orange'}
+        plt.plot(self.x_[0], self.x_[1])
+        # plt.grid()
+        # for i in range(len(self.x_)):
+        #     plt.plot(self.t_, self.x_[i], color=colors[i], label=f'x{i}(t)')
         legend = plt.legend(loc='upper left', shadow=True, fontsize='x-large')
         legend.get_frame()
         plt.show()
