caecbaf788a28f278dea11c9c1b89b7d7d3945b0
alavessi <84871525+alavessi@users.noreply.github.com>
2023-06-09 23:50:45 +0300

    Update solver.py

--- solver.py
+++ solver.py
@@ -17,7 +17,7 @@ class Solver:
         self.f_ = data['f']
         self.R_ = data['R']
 
-    def __f(self, t, x):   # f(t, x) returns [f_0(t, x), f_1(t, x), ...]
+    def __f(self, t, x):   # returns [f_0(t, x), f_1(t, x), ...]
         fun = []
         for i in range(self.n_):
             f = self.f_[i].subs('t', t)
@@ -26,7 +26,7 @@ class Solver:
             fun.append(f.evalf())
         return fun
 
-    def __find_x(self, p):
+    def __find_x(self, p):  # решение векторной внутренней задачи
         if self.t_star_ == self.a_:
             sol = solve_ivp(fun=self.__f, t_span=[self.a_, self.b_], y0=p, t_eval=np.linspace(self.a_, self.b_, 100))
             return sol.t, sol.y
@@ -39,41 +39,17 @@ class Solver:
             t = np.hstack((sol_left.t[:0:-1], sol_right.t))
             x = np.hstack((sol_left.y[:, :0:-1], sol_right.y))
             return t, x
-    '''
+
     def __rxs_matrix(self, X, t, A):  # returns A * X
         idx = max(0, min(len(A) - 1, round((t - self.a_) / (self.b_ - self.a_) * len(A)) - 1))
         return A[idx]@X
 
-    def __find_X(self, A):
+    def __find_X(self, A):  # решение матричной внутренней задачи
         sol_left = odeintw(func=self.__rxs_matrix, y0=np.eye(self.n_), t=np.linspace(self.t_star_, self.a_), args=(A,))
         sol_right = odeintw(func=self.__rxs_matrix, y0=np.eye(self.n_), t=np.linspace(self.t_star_, self.b_), args=(A,))
         return sol_left[-1], sol_right[-1]
-        # return np.vstack((sol_left[:0:-1], sol_right))
 
-    '''
-    def __rxs_matrix(self, t, x, A):  # returns A * X
-        idx = max(0, min(len(A) - 1, round((t - self.a_) / (self.b_ - self.a_) * len(A)) - 1))
-        return np.dot(A[idx], x)
-
-    def __find_X(self, A):
-        Xa, Xb = np.zeros(shape=(self.n_, self.n_)), np.zeros(shape=(self.n_, self.n_))
-        for i in range(self.n_):
-            x0 = np.zeros(self.n_)
-            x0[i] = 1
-            if self.t_star_ == self.a_:
-                sol = solve_ivp(fun=self.__rxs_matrix, t_span=[self.a_, self.b_], y0=x0, t_eval=[self.a_, self.b_], args=(A,))
-                Xa[:, i], Xb[:, i] = sol.y[:, 0], sol.y[:, 1]
-            elif self.t_star_ == self.b_:
-                sol = solve_ivp(fun=self.__rxs_matrix, t_span=[self.b_, self.a_], y0=x0, t_eval=[self.b_, self.a_], args=(A,))
-                Xa[:, i], Xb[:, i] = sol.y[:, 1], sol.y[:, 0]
-            else:
-                sol_left = solve_ivp(fun=self.__rxs_matrix, t_span=[self.t_star_, self.a_], y0=x0, t_eval=[self.a_], args=(A,))
-                sol_right = solve_ivp(fun=self.__rxs_matrix, t_span=[self.t_star_, self.b_], y0=x0, t_eval=[self.b_], args=(A,))
-                Xa[:, i] = sol_left.y.flatten()
-                Xb[:, i] = sol_right.y.flatten()
-        return Xa, Xb
-
-    def __Phi(self, x):
+    def __Phi(self, x):  # returns Φ(p) ≡ R(x(a,p), x(b,p))
         Phi = np.ndarray(self.n_)
         for i in range(self.n_):
             R = self.R_[i]
@@ -88,14 +64,14 @@ class Solver:
             A = A.subs({self.xa_[i]: x[i][0], self.xb_[i]: x[i][-1]})
         return np.array(A, dtype=float)
 
-    def __solve_inner(self, J, p):
+    def __solve_inner(self, J, p):  # решение внутренней задачи
         t, x = self.__find_x(p)
         A = []
         for i in range(len(t)):
             A.append(J.subs('t', t[i]))
             for j in range(self.n_):
                 A[i] = A[i].subs(self.x_[j], x[j][i])
-        Xa, Xb = self.__find_X(A)
+        Xa, Xb = self.__find_X(np.array(A, dtype=float))
         Rdx = Matrix(self.R_).jacobian(Matrix(self.xa_))
         Rdy = Matrix(self.R_).jacobian(Matrix(self.xb_))
         dRdx = self.__dRdx(x, Rdx)
@@ -103,12 +79,12 @@ class Solver:
         dPhidp = np.dot(dRdx, Xa) + np.dot(dRdy, Xb)
         return dPhidp
 
-    def __rhs_ext(self, mu, p, J, Phi0):
+    def __rhs_ext(self, mu, p, J, Phi0):  # returns −[Φ′(p)]^(−1) * Φ(p0)
         dPhidp = self.__solve_inner(J, p)
         print(np.linalg.det(dPhidp))
         return (-1) * np.dot(np.linalg.inv(dPhidp), Phi0)
 
-    def __solve_external(self, J, Phi0):
+    def __solve_external(self, J, Phi0):  # решение внешней задачи
         sol = solve_ivp(fun=self.__rhs_ext, t_span=[0, 1], y0=self.p0_, t_eval=[1], args=(J, Phi0))
         return sol.y.flatten()
 
